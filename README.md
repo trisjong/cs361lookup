432360c3f33175ec509eb818e6a7cd9942329cd1
ryagomes <ryagomes>
2019-03-01 11:12:37 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Website for OSU CS 361
+* Requires node/npm
+* To clone from github run `git clone https://github.com/rgomes1220/cs361lookup.git`
+* cd into the directory created
+* run `npm install` which will install dependencies from package.json
+* run node app.js to begin serving to localhost port 3000
---
+++ app.js
@@ -0,0 +1,30 @@
+// app.js
+console.log("Welcome to a sample express app!!!");
+var express=require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+//use ejs as default
+app.set("view engine","ejs")
+
+
+app.get("/", function(req, res){
+  res.render("home");
+})
+
+
+
+//404 error, route not found, this or app.get("*") at the end
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404! - The page you requested was not found :(');
+});
+
+
+
+
+app.listen(port, function(){
+  console.log('Our app is running on http://localhost:' + port);
+} );
