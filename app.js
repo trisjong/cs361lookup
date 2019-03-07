55183b2526d6ac7f1eff532a3001d9a7273e8a0b
ryagomes <ryagomes>
2019-03-06 21:56:22 -0500

    check db for authentication

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 // app.js
-console.log("Welcome to a sample express app!!!");
-var express=require("express");
-var mysql = require('./dbcon.js');
-var app = express();
-var port = process.env.PORT || 3000;
 
+var express=require("express");
+var app = express();
+var bodyParser =  require("body-parser");
+var mysql = require('./dbcon.js');
+var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended:true}));
 
 //use ejs as default
 app.set("view engine","ejs")
-
-
-
 //serve public directory
 app.use(express.static("public"));
 
@@ -20,7 +18,7 @@ app.get("/home", function(req, res){
 })
 
 app.get("/", function(req, res){
-  res.render("login");
+  res.render("login",{errorMsg:""});
 })
 
 
@@ -42,12 +40,37 @@ app.get('/get-users', function(req, res) {
 });
 
 
+
+// lookupuser
+// samplepwd
+app.post("/checkcredentials",function(req,res){
+  // console.log(req.body);
+  mysql.pool.query('SELECT * FROM lookup_users WHERE username=? and pwd =?',
+                    [req.body.inputUsername,req.body.inputPassword],
+                    function(err, results, fields) {
+                          if (err) {
+                            next(err);
+                            return;
+                          }
+                          if (results.length>0) {
+                              res.render("successful-login");
+                          } else {
+                              res.render("login",{errorMsg:"Your login credenials were incorrect!!"});
+                          }
+                    }
+  );
+
+})
+
+
+
 //404 error, route not found, this or app.get("*") at the end
 app.use(function(req,res){
   res.type('text/plain');
   res.status(404);
   res.send('404! - The page you requested was not found :(');
 });
+
 
 
 
